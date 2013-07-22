4b16ad0d358edb8dd172e9a262e2a83d48be8a34
Jean Brito <jeanfbrito@gmail.com>
2013-07-22 01:04:42 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Strtaquara::Application.config.secret_key_base = 'e0812c84fc23b1eda124f25904e74ecd84fa2247db10a750958136393122525fba3adb0ae637757db03b5445e77dab3b694aa87dfebf4f7c0c5d659e4e30bc12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Strtaquara::Application.config.session_store :cookie_store, key: '_strtaquara_session'
